46946385ea54156a26687cf8b7e73d08121025fe
Yuriy Baturin <baturinyuya@altarix.ru>
2017-07-12 10:24:54 +0500

    configure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 const moment = require('moment');
 const git = require('git-last-commit');
 const path = require('path');
+const fs = require('fs');
 moment.locale('ru');
 
 class WebpackBuildInfo {
   constructor(options = {}) {
     this.entryName = options.entryName;
     this.disabled = options.disabled || false;
+    this.PACKAGE_JSON_PATH = this.PACKAGE_JSON_PATH || './package.json';
+    this.version = null;
+
+    this._readVersion();
+  }
+
+  _readVersion() {
+    let packageFile = JSON.parse(
+      fs.readFileSync(path.resolve(this.PACKAGE_JSON_PATH), 'utf8')
+    );
+    this.version = packageFile.version;
   }
 
   apply(compiler) {
@@ -48,6 +60,7 @@ class WebpackBuildInfo {
       const buildInfo = {
         branch: commit.notes,
         lastCommitHash: commit.hash,
+        version: this.version,
         buildTime,
       };
       const result = this._getInjectString(buildInfo);
@@ -60,6 +73,7 @@ class WebpackBuildInfo {
       window.__buildInfo = ${JSON.stringify(buildInfo)};
       window.showBuild = function() {
         console.log('%c--------------------------------------------------------------', 'color: grey');
+        console.log('%Version: ' + '%c' + window.__buildInfo.version, 'color: #444444', 'color: black');
         console.log('%cBuildTime: ' + '%c' + window.__buildInfo.buildTime, 'color: #444444', 'color: black');
         console.log('%cBranch: ' + '%c' + window.__buildInfo.branch, 'color: #444444', 'color: black');
         console.log('%cLastCommitHash: ' + '%c' + window.__buildInfo.lastCommitHash, 'color: #444444', 'color: black');
